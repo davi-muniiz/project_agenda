b18e0b47467d08396d21811b26285b248408faf1
Davi Muniz <davimunizdasilvaa@gmail.com>
2025-08-11 21:32:16 -0300

    Configurando django-admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from contact.models import Contact
 
 # Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id', 'first_name', 'last_name', 'phone',    # Faz o Django Admin mostrar essas coisas que foram definidas no Contact
+    ordering = 'id',                                            # Faz o Django Admin ordenar de acordo com o parâmetro colocado.
+    # list_filter = 'created_date',                             # Cria uma 'tabela' para você selecionar filtros de pesquisa. Util ao ter muitos dados.
+    search_fields = 'id', 'first_name', 'last_name',            # Define os campos de pesquisa permitidos.
+    list_per_page = 50                                          # Máximo de dados por páginas.
+    list_max_show_all = 200                                     # Máximo de dados se pressiona show_all.
+    list_editable = 'first_name', 'last_name',                  # Permite que os campos sejam editados com nula necessidade de adentrar ao contato para tal objetivo.
+    list_display_links = 'id', 'phone',                         # Define o que será link nos campos de dados exibidos. ATENÇÃO: se o param. referido estiver em editable
+                                                                # um erro ocorrerá.
