9757fef6492702f5d752f559ba5d40d7b509f17d
Davi Muniz <davimunizdasilvaa@gmail.com>
2025-08-14 20:36:28 -0300

    Search e -se nenhum contato encontrado-

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 from contact.models import Contact
 # from django.http import Http404
 # Create your views here.
@@ -10,6 +11,33 @@ def index(request):
         'contacts': contacts,
         'title' : 'Contatos',
 
+    }
+    return render(
+        request,
+        'contact/index.html',
+        context,
+    )
+
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+    
+    contacts = Contact.objects\
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+            )\
+        .order_by('-id')[0:10]
+    context = {
+        'contacts': contacts,
+        'title' : 'Search',
+        'search_value': search_value
     }
     return render(
         request,
