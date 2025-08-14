5e5ce8488550befba6b580274bd75cd26e71892d
Davi Muniz <davimunizdasilvaa@gmail.com>
2025-08-14 19:43:09 -0300

    header, partials, site-title_relative; nav-bar, search

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -8,6 +8,8 @@ def index(request):
 
     context = {
         'contacts': contacts,
+        'title' : 'Contatos',
+
     }
     return render(
         request,
@@ -20,8 +22,10 @@ def contact(request, contact_id):
 
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True )
 
+    contact_name = f'{single_contact.first_name} {single_contact.last_name}'
     context = {
         'contact': single_contact,
+        'title': contact_name
     }
     return render(
         request,
