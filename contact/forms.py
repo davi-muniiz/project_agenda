0a74da15ec80f883e521bab2f0b8d48422ca2d05
Davi Muniz <davimunizdasilvaa@gmail.com>
2025-08-21 19:28:39 -0300

    Criando users

--- contact/forms.py
+++ contact/forms.py
@@ -1,9 +1,12 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 from . import models
 
 class ContactForm(forms.ModelForm):
+
 
     first_name = forms.CharField(               # Altera e/ou coloca campos no HTML do forms (DJANGO).
         widget= forms.TextInput(
@@ -62,3 +65,36 @@ class ContactForm(forms.ModelForm):
             )
 
         return first_name
+
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(
+        required=True,
+        min_length= 3,
+        
+    )
+    last_name = forms.CharField(
+        required=True,
+        min_length= 3,
+        
+    )
+    email = forms.EmailField(
+        required=True, 
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 'email', 'username', 'password1',
+            'password2',
+        )
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError('Já existe este e-mail!', code='invalid')
+            )
+
+        return email
--- contact/urls.py
+++ contact/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('contact/<int:contact_id>/update/', views.update, name='update'), 
     path('contact/<int:contact_id>/delete/', views.delete, name='delete'), 
     path('contact/create/', views.create, name='create'),
+    path('user/create', views.register, name='create'),
     path('search/', views.search, name='search'),
     path('', views.index, name='index' ),
 ]
