2ef65818af9ab3c7c34ddadd625759e2ade33a75
Davi Muniz <davimunizdasilvaa@gmail.com>
2025-08-24 05:15:02 -0300

    Update User

--- contact/views/users_form.py
+++ contact/views/users_form.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
-from contact.forms import RegisterForm
+from contact.forms import RegisterForm, RegisterUpdateForm
 from django.contrib import messages
-
-
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 def register(request):
     form = RegisterForm()
 
@@ -14,7 +14,7 @@ def register(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Usuário criado com sucesso')
-            return redirect('contact:index')
+            return redirect('contact:login')
 
     return render(
         request,
@@ -22,4 +22,62 @@ def register(request):
         {
             'form': form
         }
+    )
+
+def login_view(request):
+    form = AuthenticationForm(request)
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            auth.login(request, user)                       # Utiliza o sistema de login do Django para logar o user.
+            messages.success(request, 'Logado com Sucesso')
+            return redirect('contact:index')      
+        messages.error(request, 'Login Inválido!')
+
+    return render(
+        request,
+        'contact/login.html',
+        {
+            'title': 'Login',
+            'form': form,
+        }
+    )
+
+def user_update(request):
+    form = RegisterUpdateForm(instance=request.user)
+
+    if request.method != 'POST':
+        return render(
+            request,
+            'contact/user_update.html',
+            {
+                'title': 'Update',
+                'form': form,
+            }
+        )
+    
+    
+    form = RegisterUpdateForm(data=request.POST, instance=request.user)
+    if not form.is_valid():
+        return render(
+            request,
+            'contact/register.html',
+            {
+                'title': 'Update',
+                'form': form,
+            }
+        )
+    form.save()
+
+    return redirect('contact:user_update')
+   
+
+
+def logout_view(request):
+    auth.logout(request)   # Utiliza o sistema de logout do Django para deslogar o user.
+    return redirect(
+        'contact:login'
     )
